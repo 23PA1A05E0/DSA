ff870cd8d97b43e3750b29180a0738e7b69508bf
BHARATH KUMAR REDDY <160884754+23PA1A05E0@users.noreply.github.com>
2025-12-28 00:53:33 +0530

    Introduce BeingInfinity class with main method
    Added a new class BeingInfinity with a main method for input.

--- BeingInfinity.java
+++ BeingInfinity.java
@@ -1,3 +1,59 @@
+/// Time Complexity : O(n)
+
+import java.util.*;
+class Main{
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for(int i=0;i<n;i++){
+            a[i] = sc.nextInt();
+        }
+        int idx = solve(a,n);
+        System.out.println(idx);
+    }
+    
+    public static int solve(int a[], int n){
+        int ele[] = new int[n];
+        for(int i=0;i<n-1;i++){
+            int cnt = 0;
+            for(int j=i+1;j<n;j++){
+                if(a[i] > a[j])
+                    cnt++;
+            }
+            ele[i] = cnt;
+        }
+        ele[n-1] = 0;
+        int ans = 0;
+        int ite = 0;
+        for(int i=n-1;i>=0;i--){
+            ans = ans + ele[i] * ite;
+            ite++;
+        }
+        return ans + 1;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/// Time Complexity : O(NlogN)
 import java.util.*;
 class BeingInfinity{
     public static void main(String []args){
