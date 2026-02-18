e6262a52221295d0673682b0e636639eb72776ea
BHARATH KUMAR REDDY <160884754+23PA1A05E0@users.noreply.github.com>
2026-02-18 12:29:15 +0530

    Add files via upload

---
+++ TREES/SegmentTreeGCD.java
@@ -0,0 +1,78 @@
+class SegmentTreeGCD{
+    Node root;
+    public SegmentTreeGCD(int arr[]){
+        root = constructTree(arr, 0, arr.length - 1);
+    }
+
+    public Node constructTree(int arr[], int startInterval, int endInterval){
+        Node node = new Node(startInterval, endInterval);
+        if(startInterval == endInterval){
+            node.data = arr[startInterval];
+            return node;
+        }
+        int mid = startInterval + (endInterval - startInterval) / 2;
+        node.left = constructTree(arr, startInterval, mid);
+        node.right = constructTree(arr, mid + 1, endInterval);
+        node.data = gcd((node.left.data), node.right.data);
+        return node;
+    }
+
+    public int query(int startInterval, int endInterval){
+        return query(root, startInterval, endInterval);
+    }
+
+    public int query(Node root, int start, int end){
+        if(start <= root.startInterval && end >= root.endInterval)
+            return root.data;
+        if(start > root.endInterval || end < root.startInterval)
+                return 0;
+        int leftAns = query(root.left, start, end);
+        int rightAns = query(root.right, start, end);
+        return gcd(leftAns, rightAns);
+    }
+
+    public void update(int index, int value){
+        update(root, index, value);
+    }
+
+    public void update(Node root, int index, int value){
+        if(root.startInterval == root.endInterval){
+            if(root.startInterval == index){
+                root.data = value;
+            }
+            return;
+        }
+        int mid = (root.startInterval + root.endInterval) / 2;
+        if(index <= mid)
+            update(root.left, index, value);
+        else
+            update(root.right, index, value);
+
+        int left = root.left != null ? root.left.data : 0;
+        int right = root.right != null ? root.right.data : 0;
+        root.data = gcd(left, right);
+    }
+
+    private int gcd(int a, int b){
+        a = Math.abs(a);
+        b = Math.abs(b);
+        while(b != 0){
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
+
+class Node{
+    int data;
+    int startInterval;
+    int endInterval;
+    Node left;
+    Node right;
+    public Node(int si, int ei){
+        this.startInterval = si;
+        this.endInterval = ei;
+    }
+}
---
+++ TREES/SegmentTreeMax.java
@@ -0,0 +1,65 @@
+public class SegmentTreeMax {
+    Node root;
+    public SegmentTreeMax(int arr[]){
+        root = constructTree(arr, 0, arr.length - 1);
+    }
+    private Node constructTree(int[] arr, int start, int end) {
+        Node node = new Node(start, end);
+        if(start == end){
+            node.data = arr[start];
+            return node;
+        }
+        int mid =  start + (end - start) / 2;
+        node.left = constructTree(arr, start, mid);
+        node.right = constructTree(arr, mid + 1, end);
+        node.data = Math.max(node.left.data, node.right.data);
+        return node;
+    }
+
+    public int query(int si, int ei){
+        return query(root, si, ei);
+    }
+
+    private int query(Node root, int si, int ei){
+        if(si <= root.startInterval && ei >= root.endInterval)
+            return root.data;
+        if(si > root.endInterval || ei < root.startInterval)
+            return Integer.MIN_VALUE;
+        int leftAns = query(root.left, si, ei);
+        int rightAns = query(root.right, si, ei);
+        return Math.max(leftAns, rightAns);
+    }
+
+    public void update(int idx, int value){
+        update(root, idx, value);
+    }
+
+    private void update(Node root, int idx, int value){
+        if(root.startInterval == root.endInterval){
+            if(root.startInterval == idx)
+                root.data = value;
+            return;
+        }
+        int mid = root.startInterval + (root.endInterval - root.startInterval) / 2;
+        if(idx < mid)
+            update(root.left, idx, value);
+        else
+            update(root.right, idx, value);
+        int left = root.left != null ? root.left.data : Integer.MIN_VALUE;
+        int right = root.right != null ? root.right.data : Integer.MIN_VALUE;
+
+        root.data = Math.max(left, right);
+    }
+}
+
+class Node{
+    int data;
+    int startInterval;
+    int endInterval;
+    Node left;
+    Node right;
+    public Node(int si, int ei){
+        this.startInterval = si;
+        this.endInterval = ei;
+    }
+}
