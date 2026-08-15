99613abf5b306f9ca96a551f9e49aea0d3fcc633
BHARATH KUMAR REDDY <160884754+23PA1A05E0@users.noreply.github.com>
2026-08-15 21:24:29 +0530

    Update KthAncestor.java

--- TREES/KthAncestor.java
+++ TREES/KthAncestor.java
@@ -69,3 +69,125 @@ class Solution {
         countNodes(root.right);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//Special Edition
+
+class Solution {
+    int parent[];
+    int n;
+    int dp[][];
+    int depth[];
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode pa, TreeNode qa) {
+        if (root == null)
+            return root;
+
+        n = 0;
+        cntNodes(root);
+
+        parent = new int[n];
+        depth = new int[n];
+
+        Arrays.fill(parent, -1);
+
+        buildParent(root, -1, 0);
+
+        int col = (int)(Math.log(n) / Math.log(2)) + 1;
+
+        dp = new int[n][col];
+
+        for (int i = 0; i < n; i++)
+            dp[i][0] = parent[i];
+
+        for (int j = 1; j < col; j++) {
+            for (int node = 0; node < n; node++) {
+                if (dp[node][j - 1] != -1)
+                    dp[node][j] = dp[dp[node][j - 1]][j - 1];
+                else
+                    dp[node][j] = -1;
+            }
+        }
+
+        int p = pa.val;
+        int q = qa.val;
+
+        // Make p the deeper node
+        if (depth[p] < depth[q]) {
+            int temp = p;
+            p = q;
+            q = temp;
+        }
+
+        // Bring p to same depth as q
+        int diff = depth[p] - depth[q];
+
+        for (int j = 0; j < col; j++) {
+            if ((diff & (1 << j)) != 0)
+                p = dp[p][j];
+        }
+
+        // Same node
+        if (p == q)
+            return findNode(root, p);
+
+        // Lift both nodes
+        for (int j = col - 1; j >= 0; j--) {
+            if (dp[p][j] != dp[q][j]) {
+                p = dp[p][j];
+                q = dp[q][j];
+            }
+        }
+
+        // Parent of p and q is LCA
+        int lca = parent[p];
+
+        return findNode(root, lca);
+    }
+
+    public TreeNode findNode(TreeNode root, int val) {
+        if (root == null)
+            return null;
+
+        if (root.val == val)
+            return root;
+
+        TreeNode left = findNode(root.left, val);
+
+        if (left != null)
+            return left;
+
+        return findNode(root.right, val);
+    }
+
+    public void cntNodes(TreeNode root) {
+        if (root == null)
+            return;
+
+        n++;
+
+        cntNodes(root.left);
+        cntNodes(root.right);
+    }
+
+    public void buildParent(TreeNode root, int par, int d) {
+        if (root == null)
+            return;
+
+        parent[root.val] = par;
+        depth[root.val] = d;
+
+        buildParent(root.left, root.val, d + 1);
+        buildParent(root.right, root.val, d + 1);
+    }
+}
